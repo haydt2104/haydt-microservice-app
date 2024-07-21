c7e31a87cb3eb0811650bb698b7c56fc922a7868
haydt2104 <144002142+haydt2104@users.noreply.github.com>
2024-07-22 04:55:26 +0700

    product service with golang and swagger basic build

---
+++ product-service/internal/handlers/test_handler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// TestHandler godoc
+// @Summary Test API
+// @Description Test API
+// @Tags Test
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /test [get]
+func TestHandler(c *gin.Context) {
+    response := map[string]interface{}{
+        "status":  "success",
+        "message": "Hello, World!",
+    }
+
+    c.JSON(http.StatusOK, response)
+}
+
---
+++ product-service/internal/handlers/user_handler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"product-service/internal/models"
+	"product-service/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GetAllUsersHandler godoc
+// @Summary Get all users
+// @Description Retrieve all users from the database
+// @Tags Users
+// @Produce json
+// @Success 200 {array} models.User
+// @Failure 500 {object} map[string]interface{}
+// @Router /user [get]
+func GetAllUsersHandler(c *gin.Context) {
+    var users []*models.User
+    users, err := services.GetAllUsers()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+        return
+    }
+
+    c.JSON(http.StatusOK, users)
+}
