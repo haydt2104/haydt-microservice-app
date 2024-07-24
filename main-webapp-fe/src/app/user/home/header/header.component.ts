9f7573ef44d8b8c0712fddba444e3c7584d8eba6
haydt2104 <144002142+haydt2104@users.noreply.github.com>
2024-07-24 20:08:16 +0700

    update

--- main-webapp-fe/src/app/user/home/header/header.component.ts
+++ main-webapp-fe/src/app/user/home/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgbDropdownModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- main-webapp-fe/src/app/user/home/home.component.ts
+++ main-webapp-fe/src/app/user/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterLink, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
